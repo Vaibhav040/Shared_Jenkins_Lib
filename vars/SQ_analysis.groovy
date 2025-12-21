103b9157c1467881f40f97a4d7f575515c641455
Vaibhav chauhan <75163092+Vaibhav040@users.noreply.github.com>
2025-12-21 21:14:14 +0530

    Update SQ_analysis.groovy

--- vars/SQ_analysis.groovy
+++ vars/SQ_analysis.groovy
@@ -1,5 +1,5 @@
 def call(String SonarQubeAPI, String ProjectName, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
-    sh"$SONAR_HOME/bin/sonar-scanner -Dsoanr.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey} -X"
+    sh"$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey} -X"
   }
 }
